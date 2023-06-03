68296b9aa1a4fcd1598f404bbf678eeb96038d55
Jonatas S. Oliveira <dev.jonatas.oliveira@gmail.com>
2023-06-03 13:06:00 +0000

    FTZ-13: Update user validators

--- fytnez_backend/register/models/user.py
+++ fytnez_backend/register/models/user.py
@@ -1,10 +1,11 @@
 from django.db import models
+from fytnez_backend.register.validators.user_validator import UserValidator
 
 
 class User(models.Model):
     name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
-    height = models.IntegerField()
-    weight = models.IntegerField()
-    birthday = models.DateField()
+    height = models.IntegerField(validators=[UserValidator.validate_not_zero])
+    weight = models.IntegerField(validators=[UserValidator.validate_not_zero])
+    birthday = models.DateField(validators=[UserValidator.validate_bithday])
