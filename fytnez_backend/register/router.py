e9272a20b5e542e1cdb210552d1337f43d22079a
Luis Fernando Brasil <luis4756@live.com>
2023-07-03 20:34:49 -0300

    FTZ-36: Added exercises models

--- fytnez_backend/register/router.py
+++ fytnez_backend/register/router.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
-
+from fytnez_backend.register.views.exercise_view import ExerciseView
 from fytnez_backend.register.views.user_view import UserView
 
 router = routers.DefaultRouter()
 router.register(r"users", UserView)
+router.register(r"exercises", ExerciseView)
