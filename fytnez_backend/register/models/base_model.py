db9b01692b4953b2b2f376f21fd09755f740f36d
Santyero <santyero@gmail.com>
2023-09-03 17:56:20 -0300

    feature/FTZ-21 Correções criação de senhas e model admin

--- fytnez_backend/register/models/base_model.py
+++ fytnez_backend/register/models/base_model.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 class BaseModel(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
-    delete_at = models.DateTimeField()
+    delete_at = models.DateTimeField(null=True,blank=True)
 
     class Meta:
         abstract = True
