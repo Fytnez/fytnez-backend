059b1105a325c1a72da2656cf65fdee7d2c22137
Luis Fernando Brasil <luis4756@live.com>
2023-09-19 22:23:00 -0300

    feat: added UserConfig to router

--- fytnez_backend/register/router.py
+++ fytnez_backend/register/router.py
@@ -1,10 +1,11 @@
 from rest_framework import routers
 from fytnez_backend.register.views.hydric_consumption_view import HydricConsumptionView
-
 from fytnez_backend.register.views.user_view import UserView
 from fytnez_backend.register.views.achievement_view import AchievementView
+from fytnez_backend.register.views.user_config_view import UserConfigView
 
 router = routers.DefaultRouter()
 router.register(r"users", UserView)
 router.register(r"achievements", AchievementView)
 router.register(r"hydric_consumption",HydricConsumptionView)
+router.register(r"user_config", UserConfigView)
