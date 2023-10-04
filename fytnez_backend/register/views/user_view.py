340f1b217a2e5ff2c02ce519f011febad870d1bf
Luis Fernando Brasil <luis4756@live.com>
2023-10-03 21:56:04 -0300

    FTZ-43 - Added JWT middleware
    # Conflicts:
    #	fytnez_backend/register/models/user.py
    #	fytnez_backend/register/views/user_view.py
    #	fytnez_backend/urls.py

--- fytnez_backend/register/views/user_view.py
+++ fytnez_backend/register/views/user_view.py
@@ -1,9 +1,38 @@
-from rest_framework import viewsets
-
-from fytnez_backend.register.models.user import User
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from fytnez_backend.register.decorators.user_decorators import token_required
 from fytnez_backend.register.serializers.user_serializer import UserSerializer
-
+from fytnez_backend.register.models.user import User
+from fytnez_backend.register.utils.jwt_utils import generate_token, decode_jwt_token
 
 class UserView(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    
+    @api_view(['POST'])
+    def login(request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = User.objects.filter(username=username).first()
+        if user is None or not user.check_password(password):
+             return Response({'error': 'Credenciais inválidas'}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        token = generate_token(user)
+        return Response({'token': token})
+    
+    @api_view(['POST'])
+    def validate_token(request):
+        token = request.data.get('token')
+
+        if not token:
+            return Response({'error': 'Token não fornecido'}, status=status.HTTP_400_BAD_REQUEST)
+
+        payload = decode_jwt_token(token)
+
+        if payload is None:
+            # O token é inválido
+            return Response({'valid': False, 'error': 'Token inválido'}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            # O token é válido
+            return Response({'valid': True}, status=status.HTTP_200_OK)
